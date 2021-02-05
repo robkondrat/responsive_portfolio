981e4301de577e82debbc548d3b50f9ec36d68cf
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-02-05 15:08:56 -0600

    added typed effect, scroll up btn, menu select scrolls page

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ $(document).ready(function(){
     } else {
       $('.navbar').removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
   });
+
+  // slide-up script 
+  $('.scroll-up-btn').click(function() {
+    $('html').animate({scrollTop: 0});
+  })
 
   // toggle menu / navbar script 
   $('.menu-btn').click(function(){
@@ -13,6 +23,20 @@ $(document).ready(function(){
     $('.menu-btn i').toggleClass("active");
 
   });
+
+  // typing animation script 
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "Freelancer", "Illustrator", "Musician", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Freelancer", "Illustrator", "Musician", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
 
   // owl carousel script 
   $('.carousel').owlCarousel({
